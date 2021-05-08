6b68923ec72b7e2d8f2a6bf9210d8f5fac739d91
Zach Roberts <zach@eaiti.com>
2021-05-08 12:44:40 -0400

    great start

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
+import { faLinkedin, faAmazon } from '@fortawesome/free-brands-svg-icons';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'mom-book';
+  faLinkedin = faLinkedin;
+  faAmazon = faAmazon;
+
+  goToLink(url: string) {
+    window.open(url, '_blank');
+  }
 }
